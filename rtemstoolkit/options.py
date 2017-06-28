dbc53530cf7c742a1a51cf59843a01b2cc14e70c
Cillian O'Donnell <cpodonnell8@gmail.com>
2017-06-28 19:09:36 +0100

    rtemstoolkit/options.py: Add coverage option.

--- rtemstoolkit/options.py
+++ rtemstoolkit/options.py
@@ -108,6 +108,7 @@ class command_line(object):
             '--keep-going'     : ('_keep_going',       self._lo_bool,     False, '0',       True),
             '--always-clean'   : ('_always_clean',     self._lo_bool,     False, '0',       True),
             '--no-install'     : ('_no_install',       self._lo_bool,     False, '0',       True),
+            '--coverage'       : ('_coverage',         self._lo_bool,     False, '0',       False),
             '--help'           : (None,                self._lo_help,     False, None,      False)
         }
         self.long_opts_help = {
@@ -122,6 +123,7 @@ class command_line(object):
             '--jobs=[0..n,none,half,full]': 'Run with specified number of jobs, default: num CPUs.',
             '--macros file[,file]':         'Macro format files to load after the defaults',
             '--log file':                   'Log file where all build output is written to',
+            '--coverage':                   'Perform coverage analysis',
         }
         self.opts = { 'params' : [] }
         self.command_path = command_path
@@ -421,6 +423,9 @@ class command_line(object):
 
     def no_install(self):
         return self.opts['no-install'] != '0'
+
+    def coverage(self):
+        return self.opts['coverage'] != '0'
 
     def user_macros(self):
         #
